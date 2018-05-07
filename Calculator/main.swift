006d551c89a6a3a70592446ff41ec9a63339de72
Unknown <cap.noer@gmail.com>
2018-05-07 13:55:45 +0200

    Fixed readline to look more clean.

--- Calculator/main.swift
+++ Calculator/main.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Mercantec. All rights reserved.
 //
 
-// Simple calculator, used for our school project.
+// Simple calculator, used for our school project purposes.
 import Foundation
 
 
@@ -35,19 +35,17 @@ print(Multiply(numberOne: 2.5, numberTwo: 4));
 
 print(Minus(numberOne: 10.5, numberTwo: 5.5));
 
-
+print(Addition(additionOne: 1.1, additionTwo: 1.1))
 
 
 
 
 func Readline() {
     if let writtenNumbers = readLine() {
-        let writtenNumbers = Int(writtenNumbers)
-        
-        
+        _ = Double(writtenNumbers)
         let optionalDouble : Double? = usedNumber
         if let myDouble = optionalDouble {
-            var myDouble = Double(myDouble)
+            _ = Double(myDouble)
         }
     }
 }
