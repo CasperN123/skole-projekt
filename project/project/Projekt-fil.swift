6c441eb090cec4a7a1a7941bf70772e752290b63
Unknown <cap.noer@gmail.com>
2018-05-07 13:28:52 +0200

    Added ReadLine function

--- project/project/Projekt-fil.swift
+++ project/project/Projekt-fil.swift
@@ -11,9 +11,12 @@ import Foundation
 
 var additionInt = 1
 var finalResult = Int()
+var usedNumber = Double()
 
 
-func minus(numberOne: Double, nunberTwo: Double) -> Double {
+
+
+func Minus(numberOne: Double, nunberTwo: Double) -> Double {
     return (numberOne - nunberTwo)
 }
 
@@ -26,7 +29,7 @@ func Addition(additionOne: Double, additionTwo: Double) -> Double {
 
 print(minus(numberOne: 10.5, numberTwo: 5.5))
 
-func multiply(numberOne: Double, numberTwo: Double) -> Double {
+func Multiply(numberOne: Double, numberTwo: Double) -> Double {
     return ("The result of \(numberOne) * \(numberTwo) = \(numberOne * numberTwo)");
 }
 
@@ -35,3 +38,14 @@ print(multiply(numberOne: 2.5, numberTwo: 4));
 
 
 
+func Readline() {
+if let writtenNumbers = readLine() {
+    let writtenNumbers = Int(writtenNumbers)
+    
+    
+    let optionalDouble : Double? = usedNumber
+    if let myDouble = optionalDouble {
+    var myDouble = Double(myDouble)
+    }
+}
+}
